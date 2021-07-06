074d0816e603436a6d62e3c595a151c5bfad25f3
Zhaoliang <zhz03@g.ucla.edu>
2021-07-06 01:42:41 -0700

    run visualize program and save test data

--- Visualize.py
+++ Visualize.py
@@ -1,13 +1,23 @@
 import Optitrack as OptiT
+import numpy as np
 import time
+import cv2.cv2 as cv2
 
 if __name__ == '__main__':
     op = OptiT.OptiTrack()
 
     current_time = time.time()
-
+    position = []
+    begin_time = time.time()
     while True:
+
         pass
-        if time.time() - current_time >= 0.1:
-            print('position = %s' % (op.position))
+        if time.time() - current_time >= 0.1: # 0.1 second refresh rate
+            #print('position = %s' % (op.position))
+            position.append(op.position)
             current_time = time.time()
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        if time.time() - begin_time >= 5:
+            np.save('position.npy',position)
+            print("save .npy done")
+            break
