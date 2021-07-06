7ed1aaa1a4e7438afc64591557274d2dbc5ad1fc
Zhaoliang <zhz03@g.ucla.edu>
2021-07-06 01:09:54 -0700

    test original code

---
+++ Visualize.py
@@ -0,0 +1,13 @@
+import Optitrack as OptiT
+import time
+
+if __name__ == '__main__':
+    op = OptiT.OptiTrack()
+
+    current_time = time.time()
+
+    while True:
+        pass
+        if time.time() - current_time >= 0.1:
+            print('position = %s rotation = %s' % (op.position))
+            current_time = time.time()
