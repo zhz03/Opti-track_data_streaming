735b7f58440e399dbc280b3b848f63d0ec73f51d
Zhaoliang <zhz03@g.ucla.edu>
2021-07-14 02:10:06 -0700

    add velocity

--- save_livedata.py
+++ save_livedata.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
 
     current_time = time.time()
     position = []
+    velocity = []
     begin_time = time.time()
     while True:
 
@@ -15,11 +16,13 @@ if __name__ == '__main__':
         if time.time() - current_time >= 0.1: # 0.1 second refresh rate
             #print('position = %s' % (op.position))
             position.append(op.position)
+            velocity.append(op.velocity)
             current_time = time.time()
         #if cv2.waitKey(1) & 0xFF == ord('q'):
-        if time.time() - begin_time >= 10:
+        if time.time() - begin_time >= 5:
             now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
             #np.save('position.npy',position)
-            np.save(f'data/{now}.npy', position)
+            np.save(f'data/pos_{now}.npy', position)
+            np.save(f'data/vel_{now}.npy', velocity)
             print("save .npy done")
             sys.exit()
