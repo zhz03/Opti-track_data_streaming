50492c0891d3b5cb76ce661ba33402628829d220
Zhaoliang <zhz03@g.ucla.edu>
2021-08-28 21:33:04 -0700

    save data to another file

--- save_livedata.py
+++ save_livedata.py
@@ -13,16 +13,16 @@ if __name__ == '__main__':
     while True:
 
         pass
-        if time.time() - current_time >= 0.1: # 0.1 second refresh rate
+        if time.time() - current_time >= 0.01: # 0.1 second refresh rate
             #print('position = %s' % (op.position))
             position.append(op.position)
             velocity.append(op.velocity)
             current_time = time.time()
         #if cv2.waitKey(1) & 0xFF == ord('q'):
-        if time.time() - begin_time >= 5:
+        if time.time() - begin_time >= 10:
             now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
             #np.save('position.npy',position)
-            np.save(f'data/pos_{now}.npy', position)
-            np.save(f'data/vel_{now}.npy', velocity)
+            np.save(f'1_pos_time/pos_{now}.npy', position)
+            np.save(f'1_pos_time/vel_{now}.npy', velocity)
             print("save .npy done")
             sys.exit()
