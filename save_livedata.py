ae903299aa550051381f766eb6198e8491309e46
Zhaoliang <zhz03@g.ucla.edu>
2021-09-04 16:18:30 -0700

    save the time variable

--- save_livedata.py
+++ save_livedata.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     current_time = time.time()
     position = []
     velocity = []
-    time = []
+    Time = []
         
     current_time = time.time()
     begin_time = time.time()
@@ -18,16 +18,17 @@ if __name__ == '__main__':
         pass
         if time.time() - current_time >= 0.01: # 0.1 second refresh rate
             #print('position = %s' % (op.position))
-            time.append(current_time-begin_time)
+            Time.append(current_time-begin_time)
             position.append(op.position)
             velocity.append(op.velocity)
             current_time = time.time()
+            
         #if cv2.waitKey(1) & 0xFF == ord('q'):
-        if time.time() - begin_time >= 10:
+        if time.time() - begin_time >= 20:
             now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
             #np.save('position.npy',position)
             np.save(f'1_pos_time/pos_{now}.npy', position)
             np.save(f'1_pos_time/vel_{now}.npy', velocity)
-            np.save(f'1_pos_time/time_{now}.npy', time)
+            np.save(f'1_pos_time/time_{now}.npy', Time)
             print("save .npy done")
             sys.exit()
